1c703c23c004cf2afb57e8e83a5da1ca0ad0e0bd
DP <douglaspatton@gmail.com>
2020-04-01 16:14:21 -0400

    updates

--- island.py
+++ island.py
@@ -17,21 +17,143 @@ class IslandData(DataView):
         handlername=os.path.join(logdir,f'island.log')
         logging.basicConfig(
             handlers=[logging.handlers.RotatingFileHandler(os.path.join(logdir,handlername), maxBytes=10**6, backupCount=20)],
-            level=logging.DEBUG,
+            level=logging.INFO,
             format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
             datefmt='%Y-%m-%dT%H:%M:%S')
         self.logger = logging.getLogger(handlername)
-        
+        self.figdict={}
+        self.TSHistogramlist=[]
         self.datadir=os.path.join(os.getcwd(),'data')
         self.helper=Helper()
         self.datadir=os.path.join(os.getcwd(),'data')
         self.datadictlistpath=os.path.join(self.datadir,'datadictlist.pickle')
-        
-        self.subplotcount=0
+        '''self.var_type_dict={
+            'sale_year':np.int16,
+            'saleprice':np.int64,
+            'assessedvalue':np.int64,
+            'secchi':np.float64,
+            'wateraccess':np.bool,
+            'bayfront':np.bool,
+            'waterhouse':np.bool,
+            'shorelinedistance':np.float32
+        }
+        self.varlist=[key for key in self.var_type_dict]'''
+        self.varlist=[
+            'sale_year',
+            'saleprice',
+            'assessedvalue',
+            'secchi',
+            'wateraccess',
+            'bayfront',
+            'waterhouse',
+            'shorelinedistance'
+            ]
         self.fig=None;self.ax=None
         DataView.__init__(self)
         
         
+    def make2dHistogram(self,):   
+        try:self.time_arraylist
+        except: self.makeTimeArrayList()
+        time_arraylist=self.time_arraylist
+        varlist=self.varlist
+        t_idx=varlist.index('sale_year')
+        timelenlist=[_array.shape[0] for _array in self.time_arraylist]
+        xlabel=f'Sale Years {self.timelist[0]}-{self.timelist[-1]}'
+        ylabel='Count'
+        title='Count of Sales by Year'
+        fig=self.my2dbarplot(
+            np.array(self.timelist),
+            np.array(timelenlist),
+            xlabel=xlabel,ylabel=ylabel,title=title,fig=None,subplot_idx=(1,1,1))
+        if not 'hist2d' in self.figdict:
+            self.figdict['hist2d']=[fig]
+        else:
+            self.figdict['hist2d'].append(fig)
+    
+    def makeIndividualTSHistogram(self,varlist=None):
+        if not varlist:
+            varlist=self.varlist
+        for var in varlist:
+            self.makeTSHistogram(varlist=[var])
+        
+    
+    def makeTSHistogram(self,varlist=None):
+        try:self.time_arraylist
+        except: self.makeTimeArrayList()
+        time_arraylist=self.time_arraylist
+        if not varlist:
+            varlist=self.varlist
+            var_idx_list=list(range(len(varlist)))
+        else:
+            var_idx_list=[self.varlist.index(var) for var in varlist]
+        
+        #subplot_idx=[len(varlist),1,1]
+        for idxidx,var in enumerate(varlist):
+            if not var=='sale_year':
+                var_idx=var_idx_list[idxidx]
+                fig,histdict=self.my3dHistogram([nparray[:,var_idx] for nparray in time_arraylist],var,subplot_idx=(1,1,1))
+                self.TSHistogramlist.append({'histTS':histdict})
+                    
+                #subplot_idx[2]+=1
+        if not 'histTS' in self.figdict:
+            self.figdict['histTS']=[fig]
+        else:
+            self.figdict['histTS'].append(fig)
+      
+        
+    def doDictListToNpTS(self,datadictlist,timevar='sale_year'):
+        '''
+        timelist dims(obs,var)
+        '''
+        varlist=self.varlist
+        timelist=[];whichdictdict={}
+        for d_idx,datadict in enumerate(datadictlist):
+            for time in datadict[timevar]:
+                if not time in whichdictdict:
+                    whichdictdict[time]=d_idx#assumes a time can't be in both dicts
+                if not time in timelist:
+                    timelist.append(time)
+        timecount=len(timelist)
+        timelist.sort()
+        self.timelist=timelist
+        self.logger.info(f'timecount:{timecount},timelist:{timelist}')
+        timeposdict={time:i for i,time in enumerate(timelist)}
+        time_arraylist=[]
+        for time in timelist:
+            d_idx=whichdictdict[time]
+            datadict=datadictlist[d_idx]
+            thistime_idxlist=[idx for idx,idx_time in enumerate(datadict[timevar]) if idx_time==time]
+            
+            
+                    
+            obsdata=[[datadict[varkey][idx] for varkey in varlist]for idx in thistime_idxlist]
+            time_arraylist.append(np.array(obsdata,dtype=np.float64))
+        self.logger.info(f'time_arraylist shapes:{[_array.shape for _array in time_arraylist]}')
+        return time_arraylist
+    
+    
+            
+    def unpackOrderedDict(self,odict):
+        pydict={}
+        rowcount=len(odict)
+        for r_idx,row in enumerate(odict):
+            for key in row:
+                val=row[key]
+                if key in pydict:
+                    pydict[key][r_idx]=val
+                else: 
+                    pydict[key]=[None for _ in range(rowcount)]
+                    pydict[key][r_idx]=val
+        return pydict
+        
+    def makeTimeArrayList(self,):
+        try: self.datadictlist
+        except:self.pickleDataDictList() 
+        time_arraylist=self.doDictListToNpTS(self.datadictlist,timevar='sale_year') #(time,obs,var) #also creates self.timelist
+        self.time_arraylist=time_arraylist
+    
+    
     def getDataFromCSV(self,):
         pre_sandy_path=os.path.join(self.datadir,'PreSandy.csv')
         post_sandy_path=os.path.join(self.datadir,'PostSandy.csv')
@@ -49,6 +171,10 @@ class IslandData(DataView):
         self.pickleDataDictList(datadictlist=self.datadictlist)
         
     def pickleDataDictList(self,datadictlist=None):
+        '''
+        saves if  datadictlist
+        else loads
+        '''
         opentype='rb'
         if datadictlist:
             opentype='wb'
@@ -66,72 +192,3 @@ class IslandData(DataView):
             else:
                 return self.doCSVToDict()
         return 
-    
-    def doDictListToNpTS(self,datadictlist,varlist,timevar='sale_year'):
-        '''
-        timelist dims(obs,var)
-        '''
-        
-        timelist=[];whichdictdict={}
-        for d_idx,datadict in enumerate(datadictlist):
-            for time in datadict[timevar]:
-                if not time in whichdictdict:
-                    whichdictdict[time]=d_idx#assumes a time can't be in both dicts
-                if not time in timelist:
-                    timelist.append(time)
-        timecount=len(timelist)
-        timelist.sort()
-        self.timelist=timelist
-        self.logger.info(f'timecount:{timecount},timelist:{timelist}')
-        
-        timeposdict={time:i for i,time in enumerate(timelist)}
-        time_arraylist=[]
-        for time in timelist:
-            d_idx=whichdictdict[time]
-            datadict=datadictlist[d_idx]
-            thistime_idxlist=[idx for idx,idx_time in enumerate(datadict[timevar]) if idx_time==time]
-            obsdata=[[datadict[varkey][idx] for varkey in varlist]for idx in thistime_idxlist]
-            time_arraylist.append(np.array(obsdata))
-        self.logger.info(f'time_arraylist shapes:{[_array.shape for _array in time_arraylist]}')
-        return time_arraylist
-            
-            
-    def unpackOrderedDict(self,odict):
-        pydict={}
-        rowcount=len(odict)
-        for r_idx,row in enumerate(odict):
-            for key in row:
-                val=row[key]
-                if key in pydict:
-                    pydict[key][r_idx]=val
-                else: 
-                    pydict[key]=[None for _ in range(rowcount)]
-                    pydict[key][r_idx]=val
-        return pydict
-        
-    def makeTSHistogram(self,varlist=None,yearlist=None):
-        try: self.datadictlist
-        except:self.pickleDataDictList() 
-        if not varlist:
-            varlist=[
-                'sale_year',
-                'saleprice',
-                'assessedvalue',
-                'secchi',
-                'wateraccess',
-                'bayfront',
-                'waterhouse',
-                'shorelinedistance' 
-                ]
-        time_arraylist=self.doDictListToNpTS(self.datadictlist,varlist,timevar='sale_year') #(time,obs,var) #also creates self.timelist
-        t_idx=varlist.index('sale_year')
-        timelenlist=[_array.shape[0] for _array in time_arraylist]
-        xlabel=f'Sale Years {self.timelist[0]}-{self.timelist[-1]}'
-        ylabel='frequency'
-        title='Frequency of Sales by Year'
-        self.my2dbarplot(
-            np.array(self.timelist),
-            np.array(timelenlist),
-            xlabel=xlabel,ylabel=ylabel,title=title)
-        #self.fig
-      
