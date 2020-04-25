ef65430ed74fb720758557c100427712e7fbc59f
Douglas Patton <douglaspatton@gmail.com>
2020-04-25 07:27:12 -0400

    update

--- runSEM.py
+++ runSEM.py
@@ -1,9 +1,16 @@
 import island
+import pickle
 
+def savepickle(thing,path):
+    with open(path,'wb') as f:
+        pickle.dump(thing,f)
+        
 if __name__==__main__:
     idata_obj=island.IslandData()
     idata_obj.makeTimeListArrayList()
     idata_obj.addRealByCPI(to_year=2015)
     idata_obj.arrayListToPandasDF()
     resultslistlist=idata_obj.runSpatialModel()
+    savepickle(resultslistlist,'resultlislist.pickle')
+    savepickle(idata_obj,'idata_obj.pickle')
     
