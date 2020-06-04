c52c1b031b3a27de9ac3422014f51e0102084855
DP <douglaspatton@gmail.com>
2020-06-04 08:52:45 -0400

    edits

--- models.py
+++ models.py
@@ -106,7 +106,7 @@ class SpatialModel():
         self.logger.info(f'x.shape:{x.shape}')
         self.logger.info(f'x:{x}')
         args=[y,x,w]
-        kwargs={'name_y':yvar,'name_x':xvarlist,'name_w':f'inv_dist_nn{nn}','spat_diag':True,}
+        kwargs={'name_y':yvar,'name_x':xvarlist,'name_w':f'nn{nn}','spat_diag':True,}
         hashable_key=[args[:2],kwargs] # w probably not hashable, so exclude
         trysavedsem=self.checkForSaveHash(hashable_key,filestring='_sem')
         if trysavedsem:
