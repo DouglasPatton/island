13603a653310adb0961528b6cc2d7190c6c453b9
Douglas Patton <douglaspatton@gmail.com>
2020-06-04 16:48:23 -0400

    runmodel.py

---
+++ runModel.py
@@ -0,0 +1,18 @@
+import island
+import pickle
+import os
+
+def savepickle(thing,path):
+    with open(path,'wb') as f:
+        pickle.dump(thing,f)
+        
+if __name__=='__main__':
+    
+    
+    idata_obj=island.IslandData()
+    idata_obj.makeTimeListArrayList()
+    idata_obj.addRealByCPI()
+    idata_obj.arrayListToPandasDF()
+    idata_obj.runSpatialModel()
+    idata_obj.printSEMResults()
+    
